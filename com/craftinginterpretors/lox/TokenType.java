06c7c228afd62c6e556a19f4704c6118e5195646
Hasan <mmhasan78607@gmail.com>
2025-06-22 15:55:34 +0530

    complete scanning stage

---
+++ com/craftinginterpretors/lox/TokenType.java
@@ -0,0 +1,21 @@
+package com.craftinginterpretors.lox;
+
+enum TokenType{
+    //Single-character tokens.
+    LEFT_PAREN,RIGHT_PAREN,LEFT_BRACE,RIGHT_BRACE,
+    COMMA,DOT,MINUS,PLUS,SEMICOLON,SLASH,STAR,
+
+    //one or two character tokens.
+    BANG,BANG_EQUAL,
+    EQUAL,EQUAL_EQUAL,
+    GREATER,GREATER_EQUAL,
+    LESS,LESS_EQUAL,
+
+    //Literals.
+    IDENTIFIER,STRING,NUMBER,
+
+    //Keywords.
+    AND,CLASS,ELSE,FUN,FOR,IF,NIL,OR,PRINT,RETURN,SUPER,THIS,TRUE,VAR,WHILE,FALSE,
+
+    EOF
+}
